bef83045cbd50125add80ea32390e438e199c2b4
trfd <baptiste.dupy@gmail.com>
2014-07-06 16:01:37 +0200

    Updated version header

--- cytok/include/version.hpp
+++ cytok/include/version.hpp
@@ -2,11 +2,11 @@
 #define CK_MAJOR_VERSION 0
 #define CK_MINOR_VERSION 0
 #define CK_PATCH_VERSION 2
-#define CK_COMMIT_VERSION "g6de35c9"
-#define CK_REVISION_VERSION 10
+#define CK_COMMIT_VERSION "gc5b4515"
+#define CK_REVISION_VERSION 11
 
-#define CK_VERSION_FULL "0.0.2.1.cg6de35c9.r10"
-#define CK_VERSION "0.0.2.1"
+#define CK_VERSION_FULL "0.0.2.2.cgc5b4515.r11"
+#define CK_VERSION "0.0.2.2"
 
 
 
