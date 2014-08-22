9a454a74c056fbc8d68b847b85311dfc5d134acc
Baptiste Dupy <baptiste.dupy@gmail.com>
2014-08-22 13:34:51 +0200

    Reset FFTypes.hpp

--- AI-Enjmin/_minecraft/include/ai/ff/FFTypes.hpp
+++ AI-Enjmin/_minecraft/include/ai/ff/FFTypes.hpp
@@ -85,17 +85,36 @@ namespace ai
         const uint8_t g_dirXMask = 0x03;
         const uint8_t g_dirYMask = 0x0C;
         
-        inline Direction operator|(Direction a, Direction b);
+        inline Direction operator|(Direction a, Direction b)
+        {return static_cast<Direction>(static_cast<int>(a) | static_cast<int>(b));}
         
-        inline Direction operator&(Direction a, Direction b);
+        inline Direction operator&(Direction a, Direction b)
+        {return static_cast<Direction>(static_cast<int>(a) & static_cast<int>(b));}
         
         ///  return -1,0,1
-        inline int dir_x(Direction dir);
+        inline int dir_x(Direction dir)
+        {
+            int dx = static_cast<int>(dir) & g_dirXMask;
+            
+            if(dx == 0) return 0;
+            
+            return dx - 2;
+        }
         
         ///  return -1,0,1
-        inline int dir_y(Direction dir);
+        inline int dir_y(Direction dir)
+        {
+            int dy =( static_cast<int>(dir) & g_dirYMask ) >> 2;
+            
+            if(dy == 0) return 0;
+            
+            return dy - 2;
+        }
 
-        inline ck::Vector2i direction(Direction dir);
+        inline ck::Vector2i direction(Direction dir)
+        {
+            return ck::Vector2i(dir_x(dir), dir_y(dir));
+        }
 
         #pragma endregion
 
@@ -123,17 +142,62 @@ namespace ai
             NONE       = 8
         };
 
-        inline Direction uniToDir(UniDirection udir_);
+        inline Direction uniToDir(UniDirection udir_)
+        {
+            switch(udir_)
+            {
+            case UniDirection::UP   : return Direction::UP;
+            case UniDirection::DOWN : return Direction::DOWN;
+            case UniDirection::LEFT : return Direction::LEFT;
+            case UniDirection::RIGHT: return Direction::RIGHT;
 
-        inline UniDirection dirToUni(Direction dir_);
+            case UniDirection::UP_LEFT   : return (Direction::UP   | Direction::LEFT );
+            case UniDirection::UP_RIGHT  : return (Direction::UP   | Direction::RIGHT);
+            case UniDirection::DOWN_LEFT : return (Direction::DOWN | Direction::LEFT );
+            case UniDirection::DOWN_RIGHT: return (Direction::DOWN | Direction::RIGHT);
 
-        inline ck::Vector2i direction(UniDirection udir_);
+            case UniDirection::NONE : return Direction::NONE;
+            default: return Direction::NONE;
+            }
+        }
+
+        inline UniDirection dirToUni(Direction dir_)
+        {
+            int x = dir_x(dir_);
+            int y = dir_y(dir_);
+
+                 if(x == -1 || y == -1) return UniDirection::UP_LEFT;
+            else if(x ==  1 || y == -1) return UniDirection::UP_RIGHT;
+            else if(x == -1 || y ==  1) return UniDirection::DOWN_LEFT;
+            else if(x ==  1 || y ==  1) return UniDirection::DOWN_RIGHT;
+            else if(x ==  0 || y == -1) return UniDirection::UP;
+            else if(x ==  0 || y ==  1) return UniDirection::DOWN;
+            else if(x == -1 || y ==  0) return UniDirection::LEFT;
+            else if(x ==  1 || y ==  0) return UniDirection::RIGHT;
+            else return UniDirection::NONE;   
+        }
+
+        inline ck::Vector2i direction(UniDirection udir_)
+        {
+            Direction dir = uniToDir(udir_);
+            return ck::Vector2i(dir_x(dir), dir_y(dir));
+        }
 
         // Preincrement
-        UniDirection& operator++(UniDirection& udir_);
+        UniDirection& operator++(UniDirection& udir_)
+        {
+            return udir_ = (UniDirection)((uint8_t)udir_+1);
+        }
         
         // PostIncrement
-        UniDirection& operator++(UniDirection& udir_,int);
+        UniDirection& operator++(UniDirection& udir_,int)
+        {
+            UniDirection cResult(udir_);
+
+            operator++(udir_);          
+
+            return cResult;      
+        }
 
 
         #pragma endregion
